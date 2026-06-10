8151b4cb2e98d2ffab2e1e7444aca9bcccbde51f
Nere L <nerea.lofe@pm.me>
2026-06-10 17:47:32 +0200

    Add root redirect to products

--- src/core/router/router.tsx
+++ src/core/router/router.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Navigate, Routes, Route } from "react-router-dom";
 import { LoginPage } from "../../features/auth/login-page.tsx";
 import { ProductsPage } from "../../features/products/products-page.tsx";
 import { ProductPage } from "../../features/products/product-page.tsx";
@@ -7,6 +7,7 @@ import { NewProductPage } from "../../features/products/new-product-page.tsx";
 export function Router() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/products" />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/products" element={<ProductsPage />} />
       <Route path="/products/new" element={<NewProductPage />} />
