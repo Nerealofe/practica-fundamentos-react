e8467ae6769c2fcff1346fd6ae85ce0585518036
Nere L <nerea.lofe@pm.me>
2026-06-14 17:13:56 +0200

    Add product detail page

--- src/features/products/product-page.tsx
+++ src/features/products/product-page.tsx
@@ -1,19 +1,28 @@
 import { useParams } from "react-router-dom";
-
-const products = [
-  { id: 1, name: "Iphone" },
-  { id: 2, name: "Radio" },
-];
+import { getProductById } from "./services/products-api.ts";
+import type { Product } from "./product";
+import { useState, useEffect } from "react";
 
 export function ProductPage() {
   const params = useParams();
-  const product = products.find((product) => product.id === Number(params.id));
+  const [product, setProduct] = useState<Product | null>(null);
+  useEffect(() => {
+    if (!params.id) return;
+
+    getProductById(params.id).then((product) => {
+      setProduct(product);
+    });
+  }, [params.id]);
 
   return (
     <>
       <h1>Producto</h1>
+
       <h2>{product?.name}</h2>
-      <p>Id: {product?.id}</p>
+      <p>Precio: {product?.price} €</p>
+      <p>{product?.isOnSale ? "En oferta" : "No esta en oferta"}</p>
+      <p>Tags: {product?.tags.join(",")}</p>
+      <p>{product?.description}</p>
     </>
   );
 }
--- src/features/products/services/products-api.ts
+++ src/features/products/services/products-api.ts
@@ -1,9 +1,19 @@
+import type { Product } from "../product.ts";
+
 const API_URL = import.meta.env.VITE_API_BASE_URL;
 
 export async function getProducts() {
   const response = await fetch(`${API_URL}/api/products`);
   if (!response.ok) {
     throw new Error("Error cargando productos");
+  }
+  return response.json();
+}
+
+export async function getProductById(id: string): Promise<Product> {
+  const response = await fetch(`${API_URL}/api/products/${id}`);
+  if (!response.ok) {
+    throw new Error("Error cargando producto");
   }
   return response.json();
 }
