39ea54d93cd21fc2ad7976e8353a5ae609d3962a
Nere L <nerea.lofe@pm.me>
2026-06-14 15:48:27 +0200

    Add product detail page

--- src/features/products/components/product-card.tsx
+++ src/features/products/components/product-card.tsx
@@ -1,4 +1,5 @@
 import type { Product } from "../product";
+import { Link } from "react-router-dom";
 
 type ProductCardProps = {
   product: Product;
@@ -6,7 +7,9 @@ type ProductCardProps = {
 export function ProductCard({ product }: ProductCardProps) {
   return (
     <article>
-      <h3>{product.name}</h3>
+      <h3>
+        <Link to={`/products/${product.id}`}> {product.name} </Link>
+      </h3>
       <p>{product.price} €</p>
       <p>{product.isOnSale ? "En oferta" : "No está en oferta"}</p>
       <p>{product.tags.join(", ")}</p>
--- src/features/products/product-page.tsx
+++ src/features/products/product-page.tsx
@@ -1,3 +1,19 @@
+import { useParams } from "react-router-dom";
+
+const products = [
+  { id: 1, name: "Iphone" },
+  { id: 2, name: "Radio" },
+];
+
 export function ProductPage() {
-  return <h1>Producto</h1>;
+  const params = useParams();
+  const product = products.find((product) => product.id === Number(params.id));
+
+  return (
+    <>
+      <h1>Producto</h1>
+      <h2>{product?.name}</h2>
+      <p>Id: {product?.id}</p>
+    </>
+  );
 }
